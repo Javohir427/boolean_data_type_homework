a25cb6986b412bdbeb85b8bf82136c1d29d998b5
Javohir427 <ortiqxojayevjavohir0511@gmail.com>
2022-10-25 17:09:34 +0500

    02

--- bool02.py
+++ bool02.py
@@ -1,10 +1,18 @@
-def main(a):
-    """
-    Check the logic "The variable 'a' is equal 7"
-    Args:
-        a: int
-    Returns:
-        bool
-    """
+#def main(a):
+ #   """
+  #  Check the logic "The variable 'a' is equal 7"
+  #  Args:
+  #      a: int
+   # Returns:
+    #    bool
+    #"""
     # Write your code here
-    return 
+    #return 
+
+
+
+def main(a):
+   
+    return(a==7)
+
+print(main(7))
