9a9fd126ba7d34621f23dd4fec660eb9e82d5e1e
Javohir427 <ortiqxojayevjavohir0511@gmail.com>
2022-10-25 17:03:23 +0500

    01

--- bool01.py
+++ bool01.py
@@ -1,11 +1,15 @@
-def main(a,b):
-    """
-    Check the logic "they are equal"
-    Args:
-        a: int
-        b: int
-    Returns:
-        bool
-    """
+#def main(a,b):
+ #   """
+  #  Check the logic "they are equal"
+   # Args:
+    #    a: int
+     #   b: int
+    #Returns:
+     #   bool
+    #"""
     # Write your code here
-    return 
+   # return 
+a = int
+b = int
+
+print(a==b)
