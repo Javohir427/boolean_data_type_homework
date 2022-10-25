16b68387a1cd6ecd2ef56eb19aa4c635a0703f06
Javohir427 <ortiqxojayevjavohir0511@gmail.com>
2022-10-25 20:39:28 +0500

    05

--- bool05.py
+++ bool05.py
@@ -1,10 +1,25 @@
-def main(a):
-    """
-    Check the logic "The variable "a" is an odd number"
-    Args:
-        a: int
-    Returns:
-        bool
-    """
+#def main(a):
+ #   """
+  #  Check the logic "The variable "a" is an odd number"
+   # Args:
+    #    a: int
+    #Returns:
+     #   bool
+    #"""
     # Write your code here
-    return 
+   # return 
+
+
+
+def main(a):
+    x = a%2
+    return(x)==1
+
+
+
+
+print(main(9))
+
+
+
+
