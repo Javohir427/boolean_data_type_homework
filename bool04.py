d9c6f1496fdd767b65c32dca7154c4c383b8dfd3
Javohir427 <ortiqxojayevjavohir0511@gmail.com>
2022-10-26 08:54:58 +0500

    04

--- bool04.py
+++ bool04.py
@@ -1,10 +1,15 @@
-def main(a):
-    """
-    Check the logic "The variable "a" is negative"
-    Args:
-        a: int
-    Returns:
-        bool
-    """
+#def main(a):
+ #   """
+  #  Check the logic "The variable "a" is negative"
+   # Args:
+    #    a: int
+    #Returns:
+     #   bool
+    #"""
     # Write your code here
-    return 
+    #return 
+def main(a):
+        return(a<0)
+print(main(-5))
+
+    
